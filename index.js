57ed7ebd6f7c46cc2b6df8817e7e8c37f5dbffeb
Nathan Blair <engineer.nblair@gmail.com>
2021-03-12 12:22:35 -0500

    can almost do all kinds of crazy shit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-/** @typedef {Object.<Number, String[]>} Args */
+/** @typedef {Object.<Number, any>} Args */
 /** @typedef {Object.<Number, String[]>} TokenList */
 
 export const global_flag_index = 0
 
 /**
  * @param {string[]} args
- * @param {TokenList} token_list
+ * @param {TokenList} [token_list]
  *
  * @returns {Promise<Args>}
  * */
@@ -19,20 +19,20 @@ export async function parse(args, token_list) {
   ) {
     const current_arg_value = args[current_arg_index]
 
-    const next_arg_index = current_arg_index + 1
-    const next_arg_value = args[next_arg_index]
-
-    const next_value_is_token =
-      token_list[current_section]?.indexOf(next_arg_value) >= 0
-
-    const current_value_is_flag = current_arg_value[0] === "-"
-    const next_value_is_flag =
-      args.length > next_arg_index && next_arg_value[0] === "-"
-
-    if (parse[current_section] === undefined) parse[current_section] = {}
-
-    if (current_value_is_flag) {
+    if (current_arg_value[0] === "-") {
       let equals_index = current_arg_value.indexOf("=")
+
+      const next_arg_index = current_arg_index + 1
+      const next_arg_value = args[next_arg_index]
+
+      const next_value_is_flag =
+        args.length > next_arg_index && next_arg_value[0] === "-"
+
+      const next_value_is_token =
+        token_list !== undefined &&
+        token_list[current_section]?.indexOf(next_arg_value) >= 0
+
+      if (parse[current_section] === undefined) parse[current_section] = {}
 
       const value =
         next_value_is_flag ||
@@ -49,12 +49,13 @@ export async function parse(args, token_list) {
           : current_arg_value
         ).replace(/^-{1,2}/, "")
       ] = value
-    } else if (next_value_is_token) {
+    } else if (
+      token_list !== undefined &&
+      token_list[current_section]?.indexOf(current_arg_value) >= 0
+    ) {
+      // FIXME
       current_section += 1
     }
-
-    // FIXME If next index is not a flag then its either a value or a token
-    // If its not a token then we need to skip current_index beyond it
   }
 
   return parse
--- tests/parse.js
+++ tests/parse.js
@@ -8,69 +8,132 @@ export const assertions = {
   "Should be able to toggle a flag with the '-$flag' syntax when no value is passed": {
     function: async () => {
       const flag_name = "f"
-      const parsed = await parse([`-${flag_name}`], {})
+      const parsed = await parse([`-${flag_name}`])
       assert.deepStrictEqual(parsed[global_flag_index][flag_name], true)
     },
-    // skip: true,
+    skip: true,
   },
   "Should be able to toggle a flag with the '--$flag' syntax when no value is passed": {
     function: async () => {
       const flag_name = "flag"
-      const parsed = await parse([`--${flag_name}`], {})
+      const parsed = await parse([`--${flag_name}`])
       assert.deepStrictEqual(parsed[global_flag_index][flag_name], true)
     },
-    // skip: true,
+    skip: true,
   },
   "Should be able to set a flag value passed with '-$flag' syntax": {
     function: async () => {
       const flag_name = "f"
       const value = "value"
-      const parsed = await parse([`-${flag_name}`, value], {})
+      const parsed = await parse([`-${flag_name}`, value])
       assert.deepStrictEqual(parsed[global_flag_index][flag_name], value)
     },
-    // skip: true,
+    skip: true,
   },
   "Should be able to set a flag value passed with '--$flag' syntax": {
     function: async () => {
       const flag_name = "flag"
       const value = "value"
-      const parsed = await parse([`--${flag_name}`, value], {})
+      const parsed = await parse([`--${flag_name}`, value])
       assert.deepStrictEqual(parsed[global_flag_index][flag_name], value)
     },
-    // skip: true,
+    skip: true,
   },
   "Should be able to set a flag value passed with '--$flag=' syntax": {
     function: async () => {
       const flag_name = "flag"
       const value = "value"
-      const parsed = await parse([`--${flag_name}=${value}`], {})
+      const parsed = await parse([`--${flag_name}=${value}`])
       assert.deepStrictEqual(parsed[global_flag_index][flag_name], value)
     },
-    // skip: true,
+    skip: true,
   },
   "Should be able to toggle multiple flags with the '-$flag' syntax when no value is passed": {
     function: async () => {
       const flag1_name = "f1"
       const flag2_name = "f2"
-      const parsed = await parse([`-${flag1_name}`, `-${flag2_name}`], {})
+      const parsed = await parse([`-${flag1_name}`, `-${flag2_name}`])
       assert.deepStrictEqual(parsed[global_flag_index][flag1_name], true)
       assert.deepStrictEqual(parsed[global_flag_index][flag2_name], true)
     },
-    // skip: true,
+    skip: true,
   },
   "Should be able to toggle multiple flags with the '--$flag' syntax when no value is passed": {
     function: async () => {
-      assert.fail("Not yet implemented!")
+      const flag1_name = "flag1"
+      const flag2_name = "flag2"
+      const parsed = await parse([`--${flag1_name}`, `--${flag2_name}`])
+      assert.deepStrictEqual(parsed[global_flag_index][flag1_name], true)
+      assert.deepStrictEqual(parsed[global_flag_index][flag2_name], true)
     },
     skip: true,
   },
-  "Should be able to set multiple flags with values": {
+  "Should be able to toggle multiple flags with both '-$flag' and '--$flag' syntax when no value is passed for either": {
+    function: async () => {
+      const flag1_name = "f"
+      const flag2_name = "flag"
+      const parsed = await parse([`-${flag1_name}`, `--${flag2_name}`])
+      assert.deepStrictEqual(parsed[global_flag_index][flag1_name], true)
+      assert.deepStrictEqual(parsed[global_flag_index][flag2_name], true)
+    },
+    skip: true,
+  },
+  "Should be able to set multiple flag values using '-$flag' syntax": {
+    function: async () => {
+      const flag1_name = "f1"
+      const flag2_name = "f2"
+      const value1 = "value1"
+      const value2 = "value2"
+      const parsed = await parse([
+        `-${flag1_name}`,
+        `${value1}`,
+        `-${flag2_name}`,
+        `${value2}`,
+      ])
+      assert.deepStrictEqual(parsed[global_flag_index][flag1_name], value1)
+      assert.deepStrictEqual(parsed[global_flag_index][flag2_name], value2)
+    },
+    skip: true,
+  },
+  "Should be able to set multiple flag values using '--$flag' syntax": {
+    function: async () => {
+      const flag1_name = "f1"
+      const flag2_name = "f2"
+      const value1 = "value1"
+      const value2 = "value2"
+      const parsed = await parse([
+        `--${flag1_name}`,
+        `${value1}`,
+        `--${flag2_name}`,
+        `${value2}`,
+      ])
+      assert.deepStrictEqual(parsed[global_flag_index][flag1_name], value1)
+      assert.deepStrictEqual(parsed[global_flag_index][flag2_name], value2)
+    },
+    skip: true,
+  },
+  "Should be able to set multiple flag values using '--$flag=' syntax": {
+    function: async () => {
+      const flag1_name = "f1"
+      const flag2_name = "f2"
+      const value1 = "value1"
+      const value2 = "value2"
+      const parsed = await parse([
+        `--${flag1_name}=${value1}`,
+        `--${flag2_name}=${value2}`,
+      ])
+      assert.deepStrictEqual(parsed[global_flag_index][flag1_name], value1)
+      assert.deepStrictEqual(parsed[global_flag_index][flag2_name], value2)
+    },
+    // skip: true,
+  },
+  "Should be able to set multiple flag values using a mix of '-$flag', '--$flag', and '--$flag=' syntax": {
     function: async () => {
       assert.fail("Not yet implemented!")
     },
     skip: true,
   },
-  "Should be able to set a mix of flag toggles and values": {
+  "Should be able to set a mix of flag toggles and values using a mix of '-$flag', '--$flag', and '--$flag=' syntax": {
     function: async () => {
       assert.fail("Not yet implemented!")
     },

2dc2922e7880688d176ccda2b89eefc862e5a36c
Nathan Blair <engineer.nblair@gmail.com>
2021-03-12 16:12:06 -0500

    most of valid token with options testing done

--- tests/tokens/valid_tokens_with_token_options.js
+++ tests/tokens/valid_tokens_with_token_options.js
@@ -83,4 +83,124 @@ export const assertions = {
     },
     // skip: true,
   },
+  "Should be able to pass a valid token out of 1 token possibility with a flag value option using the '-$flag' syntax": {
+    function: async () => {
+      const flag_name = "f"
+      const value = "value"
+
+      const parsed = await parse(
+        [valid_token_name, `-${flag_name}`, value],
+        Object.freeze({
+          0: [valid_token_name],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag_name]: value,
+        },
+      })
+    },
+    // skip: true,
+  },
+  "Should be able to pass a valid token out of 2 token possibilities with a flag value option using the '-$flag' syntax": {
+    function: async () => {
+      const flag_name = "f"
+      const value = "value"
+
+      const parsed = await parse(
+        [valid_token_name, `-${flag_name}`, value],
+        Object.freeze({
+          0: [valid_token_name, "token2"],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag_name]: value,
+        },
+      })
+    },
+    // skip: true,
+  },
+  "Should be able to pass a valid token out of 1 token possibility with a flag value option using the '--$flag' syntax": {
+    function: async () => {
+      const flag_name = "f"
+      const value = "value"
+
+      const parsed = await parse(
+        [valid_token_name, `--${flag_name}`, value],
+        Object.freeze({
+          0: [valid_token_name],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag_name]: value,
+        },
+      })
+    },
+    // skip: true,
+  },
+  "Should be able to pass a valid token out of 2 token possibilities with a flag value option using the '--$flag' syntax": {
+    function: async () => {
+      const flag_name = "f"
+      const value = "value"
+
+      const parsed = await parse(
+        [valid_token_name, `--${flag_name}`, value],
+        Object.freeze({
+          0: [valid_token_name, "token2"],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag_name]: value,
+        },
+      })
+    },
+    // skip: true,
+  },
+  "Should be able to pass a valid token out of 1 token possibility with a flag value option using the '--$flag=' syntax": {
+    function: async () => {
+      const flag_name = "f"
+      const value = "value"
+
+      const parsed = await parse(
+        [valid_token_name, `--${flag_name}=${value}`],
+        Object.freeze({
+          0: [valid_token_name],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag_name]: value,
+        },
+      })
+    },
+    // skip: true,
+  },
+  "Should be able to pass a valid token out of 2 token possibilities with a flag value option using the '--$flag=' syntax": {
+    function: async () => {
+      const flag_name = "f"
+      const value = "value"
+
+      const parsed = await parse(
+        [valid_token_name, `--${flag_name}=${value}`],
+        Object.freeze({
+          0: [valid_token_name, "token2"],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag_name]: value,
+        },
+      })
+    },
+    // skip: true,
+  },
 }

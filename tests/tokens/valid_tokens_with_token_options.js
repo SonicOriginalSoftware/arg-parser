8e91ec5053d90e689f0a63a139ca81fe26be733e
Nathan Blair <engineer.nblair@gmail.com>
2021-03-12 16:15:14 -0500

    that should take care of tokens with options

--- tests/tokens/valid_tokens_with_token_options.js
+++ tests/tokens/valid_tokens_with_token_options.js
@@ -203,4 +203,82 @@ export const assertions = {
     },
     // skip: true,
   },
+  "Should be able to pass a valid token out of 1 token possibility with a mix of flag toggles and values using the '-$flag', '--$flag', and '--$flag=' syntax": {
+    function: async () => {
+      const flag1_name = "f1"
+      const flag2_name = "flag2"
+      const flag3_name = "f3"
+      const flag4_name = "flag4"
+      const flag5_name = "flag5"
+      const value3 = "value3"
+      const value4 = "value4"
+      const value5 = "value5"
+
+      const parsed = await parse(
+        [
+          valid_token_name,
+          `-${flag1_name}`,
+          `--${flag2_name}`,
+          `-${flag3_name}`,
+          value3,
+          `--${flag4_name}`,
+          value4,
+          `--${flag5_name}=${value5}`,
+        ],
+        Object.freeze({
+          0: ["token1"],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag1_name]: true,
+          [flag2_name]: true,
+          [flag3_name]: value3,
+          [flag4_name]: value4,
+          [flag5_name]: value5,
+        },
+      })
+    },
+    // skip: true,
+  },
+  "Should be able to pass a valid token out of 2 token possibilities with a mix of flag toggles and values using the '-$flag', '--$flag', and '--$flag=' syntax": {
+    function: async () => {
+      const flag1_name = "f1"
+      const flag2_name = "flag2"
+      const flag3_name = "f3"
+      const flag4_name = "flag4"
+      const flag5_name = "flag5"
+      const value3 = "value3"
+      const value4 = "value4"
+      const value5 = "value5"
+
+      const parsed = await parse(
+        [
+          valid_token_name,
+          `-${flag1_name}`,
+          `--${flag2_name}`,
+          `-${flag3_name}`,
+          value3,
+          `--${flag4_name}`,
+          value4,
+          `--${flag5_name}=${value5}`,
+        ],
+        Object.freeze({
+          0: ["token1", "token2"],
+        })
+      )
+      assert.deepStrictEqual(parsed, {
+        [1]: {
+          [token_key]: valid_token_name,
+          [flag1_name]: true,
+          [flag2_name]: true,
+          [flag3_name]: value3,
+          [flag4_name]: value4,
+          [flag5_name]: value5,
+        },
+      })
+    },
+    // skip: true,
+  },
 }

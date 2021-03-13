2b89778e3c91a01a00e5039fa3be9b2d069cd904
Nathan Blair <engineer.nblair@gmail.com>
2021-03-12 22:08:58 -0500

    that's all for single tokens

--- tests/globals_with_tokens_and_token_options/single_token/valid/global_values/single_token_option/global_values_with_valid_tokens_and_token_values.js
+++ tests/globals_with_tokens_and_token_options/single_token/valid/global_values/single_token_option/global_values_with_valid_tokens_and_token_values.js
@@ -1,394 +1,449 @@
 import { strict as assert } from "assert"
 
-import { parse, global_flag_index } from "../../../../../../index.js"
+import { parse, global_flag_index, token_key } from "../../../../../../index.js"
 
 export const id = "Global Values with Valid Tokens and Token Values"
 
-const invalid_token_name = "bogus"
+const token_index = 1
+const valid_token_name = "bogus"
 const global_flag_name = "f"
 const global_flag_value = "global"
 const token_flag_name = "g"
 const value = "value"
 
 export const assertions = {
-  "Global value set with '-$flag' syntax with invalid token out of 1 possible token and flag value option set with '-$flag' syntax": {
+  "Global value set with '-$flag' syntax with valid token out of 1 possible token and flag value option set with '-$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `-${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `-${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag' syntax with invalid token out of 1 possible token and flag value option set with '-$flag' syntax": {
+  "Global value set with '--$flag' syntax with valid token out of 1 possible token and flag value option set with '-$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `-${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag=' syntax with invalid token out of 1 possible token and flag value option set with '-$flag' syntax": {
+  "Global value set with '--$flag=' syntax with valid token out of 1 possible token and flag value option set with '-$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}=${global_flag_value}`,
-          invalid_token_name,
+          valid_token_name,
           `-${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '-$flag' syntax with invalid token out of 1 possible token and flag value option set with '--$flag' syntax": {
+  "Global value set with '-$flag' syntax with valid token out of 1 possible token and flag value option set with '--$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `-${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag' syntax with invalid token out of 1 possible token and flag value option set with '--$flag' syntax": {
+  "Global value set with '--$flag' syntax with valid token out of 1 possible token and flag value option set with '--$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag=' syntax with invalid token out of 1 possible token and flag value option set with '--$flag' syntax": {
+  "Global value set with '--$flag=' syntax with valid token out of 1 possible token and flag value option set with '--$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}=${global_flag_value}`,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '-$flag' syntax with invalid token out of 1 possible token and flag value option set with '--$flag=' syntax": {
+  "Global value set with '-$flag' syntax with valid token out of 1 possible token and flag value option set with '--$flag=' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `-${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}=${value}`,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag' syntax with invalid token out of 1 possible token and flag value option set with '--$flag=' syntax": {
+  "Global value set with '--$flag' syntax with valid token out of 1 possible token and flag value option set with '--$flag=' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}=${value}`,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag=' syntax with invalid token out of 1 possible token and flag value option set with '--$flag=' syntax": {
+  "Global value set with '--$flag=' syntax with valid token out of 1 possible token and flag value option set with '--$flag=' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}=${global_flag_value}`,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}=${value}`,
         ],
-        Object.freeze({ 0: ["token1"] })
+        Object.freeze({ 0: [valid_token_name] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '-$flag' syntax with invalid token out of 2 possible tokens and flag value option set with '-$flag' syntax": {
+  "Global value set with '-$flag' syntax with valid token out of 2 possible tokens and flag value option set with '-$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `-${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `-${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag' syntax with invalid token out of 2 possible tokens and flag value option set with '-$flag' syntax": {
+  "Global value set with '--$flag' syntax with valid token out of 2 possible tokens and flag value option set with '-$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `-${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag=' syntax with invalid token out of 2 possible tokens and flag value option set with '-$flag' syntax": {
+  "Global value set with '--$flag=' syntax with valid token out of 2 possible tokens and flag value option set with '-$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}=${global_flag_value}`,
-          invalid_token_name,
+          valid_token_name,
           `-${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '-$flag' syntax with invalid token out of 2 possible tokens and flag value option set with '--$flag' syntax": {
+  "Global value set with '-$flag' syntax with valid token out of 2 possible tokens and flag value option set with '--$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `-${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag' syntax with invalid token out of 2 possible tokens and flag value option set with '--$flag' syntax": {
+  "Global value set with '--$flag' syntax with valid token out of 2 possible tokens and flag value option set with '--$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag=' syntax with invalid token out of 2 possible tokens and flag value option set with '--$flag' syntax": {
+  "Global value set with '--$flag=' syntax with valid token out of 2 possible tokens and flag value option set with '--$flag' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}=${global_flag_value}`,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}`,
           value,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '-$flag' syntax with invalid token out of 2 possible tokens and flag value option set with '--$flag=' syntax": {
+  "Global value set with '-$flag' syntax with valid token out of 2 possible tokens and flag value option set with '--$flag=' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `-${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}=${value}`,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag' syntax with invalid token out of 2 possible tokens and flag value option set with '--$flag=' syntax": {
+  "Global value set with '--$flag' syntax with valid token out of 2 possible tokens and flag value option set with '--$flag=' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}`,
           global_flag_value,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}=${value}`,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
     },
     // skip: true,
   },
-  "Global value set with '--$flag=' syntax with invalid token out of 2 possible tokens and flag value option set with '--$flag=' syntax": {
+  "Global value set with '--$flag=' syntax with valid token out of 2 possible tokens and flag value option set with '--$flag=' syntax": {
     function: async () => {
       const parsed = await parse(
         [
           `--${global_flag_name}=${global_flag_value}`,
-          invalid_token_name,
+          valid_token_name,
           `--${token_flag_name}=${value}`,
         ],
-        Object.freeze({ 0: ["token1", "token2"] })
+        Object.freeze({ 0: [valid_token_name, "token2"] })
       )
 
       assert.deepStrictEqual(parsed, {
         [global_flag_index]: {
           [global_flag_name]: global_flag_value,
+        },
+        [token_index]: {
+          [token_key]: valid_token_name,
           [token_flag_name]: value,
         },
       })
